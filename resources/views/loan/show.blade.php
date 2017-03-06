8c86020a84b30cab89ad36388068defa06297231
robercastro <robertoicastro@gmail.com>
2017-03-06 12:49:45 +0100

    Modifications dans les layouts

--- resources/views/loan/show.blade.php
+++ resources/views/loan/show.blade.php
@@ -1,4 +1,3 @@
-
 <?php
 $encrypter = app('Illuminate\Encryption\Encrypter');
 $encrypted_token = $encrypter->encrypt(csrf_token());
@@ -34,67 +33,65 @@ $encrypted_token = $encrypter->encrypt(csrf_token());
         {{ $loan->created_at->format('l jS \\of F Y') }}
       </div>
     </div>
-
   </div>
-
 </div>
+
 <div class="panel panel-default">
-  <div class="col-sm-8 col-md-8" style="padding-left : 30px;">
-    <div class="row">
-      <div class="panel panel-default" >
-        <div class="panel-heading">Choisissez un produit</div>
-        <div class="panel-body">
-          <div class="container" style="width: 100%;" >
-            <button class="btn-success btn-sm" id="btn-add"><i class="fa fa-plus-circle"></i> Ajouter un produit</button><br/><br/>
-            <div class="alert alert-danger info" style="display:none;">
-              <ul></ul>
-            </div>
-            <div class="row">
-              <table id="example" class="display nowrap" cellspacing="0" width="100%">
-                <thead>
-                  <tr>
-                    <th>Nom du produit</th>
-                    <th>Prix</th>
+  <div class="panel-heading">Choisissez un produit</div>
+  <div class="panel-body">
+    <div class="container" style="width: 100%;" >
+      <div class="alert alert-danger info" style="display:none;">
+        <ul></ul>
+      </div>
+      <div class="row">
+        <table id="example" class="display nowrap" cellspacing="0" width="100%">
+          <thead>
+            <tr>
+              <th>Nom du produit</th>
+              <th>Ajout</th>
+              <th>Prix</th>
 
-                  </tr>
-                </thead>
-                <tbody id="product-list" name="product-list">
-                  @foreach($products as $products)
-                  <tr id="product{{ $products->id }}">
+            </tr>
+          </thead>
+          <tbody id="product-list" name="product-list">
+            @foreach($products as $products)
+            <tr id="product{{ $products->id }}">
 
-                    <td>{{ $products->name }}</td>
-                    <td>{{ $products->price }}</td>
+              <td>{{ $products->name }}</td>
 
-                  </tr>
-                  @endforeach
-                </tbody>
-              </table>
+              {!! Form::open(['url' => route('product.show', $products->id), 'method' => 'delete']) !!}
+              <td><button type="submit" class="btn-success btn-sm" id="btn-ajout-pro-pret"><i class="fa fa-plus-circle"></i> Ajouter au prêt</button></td>
+              {!! Form::close() !!}
 
-              <!-- Modal -->
-              <div id="myModal" class="modal fade" role="dialog" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                      <h4 class="modal-title" id="myModalLabel">Nouveau produit</h4>
-                    </div>
-                    <div class="modal-body">
-                      {!! Form::open(array('id' => 'frm', 'name' => 'frm', 'class' => 'form-horizontal')) !!}
-                      <input id="token" type="hidden" value="{{$encrypted_token}}">
-                      @include('product.form')
-                      {!! Form::close() !!}
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-                </div>
+              <td>{{ $products->price }}</td>
+
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4 class="modal-title" id="myModalLabel">Nouveau produit</h4>
+              </div>
+              <div class="modal-body">
+                {!! Form::open(array('id' => 'frm', 'name' => 'frm', 'class' => 'form-horizontal')) !!}
+                <input id="token" type="hidden" value="{{$encrypted_token}}">
+                @include('product.form')
+                {!! Form::close() !!}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
             </div>
-            <!-- Modal -->
           </div>
         </div>
       </div>
+      <!-- Modal -->
     </div>
   </div>
 </div>
--- resources/views/sale/show.blade.php
+++ resources/views/sale/show.blade.php
@@ -1,34 +1,104 @@
+<?php
+$encrypter = app('Illuminate\Encryption\Encrypter');
+$encrypted_token = $encrypter->encrypt(csrf_token());
+?>
 @extends('layouts.app')
-
 @section('content')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">Ma vente
-      @can('edit', $sale)
-        | <a href="{{ route('sale.edit', $sale->id) }}">Modifier</a>
-      @endcan
+@include('layouts.errors')
+<div class="panel panel-default">
+  <div class="panel-heading">Ma vente
+    @can('edit', $sale)
+    | <a href="{{ route('sale.edit', $sale->id) }}">Modifier</a>
+    @endcan
+  </div>
+
+  <div class="panel-body">
+    <h2>Id de la vente : {{ $sale->id }}</h2>
+
+    <p>Date de fin :
+      {{ Carbon\Carbon::parse($sale->date_sale)->format('d-M-Y')  }}
+    </p>
+    <p>Crée par :
+      {{ $sale->created_by }}
+    </p>
+    <p>Modifié par :
+      {{ $sale->modified_by }} en: {{ Carbon\Carbon::parse($sale->updated_at)->format('d-M-Y')  }}
+    </p>
+    <hr>
+    <div class="row">
+      <div class="col-md-6 text-right">
+        {{ $sale->created_at->format('l jS \\of F Y') }}
+      </div>
     </div>
+  </div>
+</div>
 
-    <div class="panel-body">
-      <h2>Id de la vente : {{ $sale->id }}</h2>
-
-      <p>Date de fin :
-        {{ Carbon\Carbon::parse($sale->date_sale)->format('d-M-Y')  }}
-      </p>
-      <p>Crée par :
-        {{ $sale->created_by }}
-      </p>
-      <p>Modifié par :
-        {{ $sale->modified_by }} en: {{ Carbon\Carbon::parse($sale->updated_at)->format('d-M-Y')  }}
-      </p>
-      <hr>
+<div class="panel panel-default">
+  <div class="panel-heading">Choisissez un produit</div>
+  <div class="panel-body">
+    <div class="container" style="width: 100%;" >
+      <div class="alert alert-danger info" style="display:none;">
+        <ul></ul>
+      </div>
       <div class="row">
-        <div class="col-md-6 text-right">
-          {{ $sale->created_at->format('l jS \\of F Y') }}
+        <table id="example" class="display nowrap" cellspacing="0" width="100%">
+          <thead>
+            <tr>
+              <th>Nom du produit</th>
+              <th>Ajout</th>
+              <th>Prix</th>
+
+            </tr>
+          </thead>
+          <tbody id="product-list" name="product-list">
+            @foreach($products as $products)
+            <tr id="product{{ $products->id }}">
+
+              <td>{{ $products->name }}</td>
+
+              {!! Form::open(['url' => route('product.show', $products->id), 'method' => 'delete']) !!}
+              <td><button type="submit" class="btn-success btn-sm" id="btn-ajout-pro-pret"><i class="fa fa-plus-circle"></i> Ajouter à la vente</button></td>
+              {!! Form::close() !!}
+
+              <td>{{ $products->price }}</td>
+
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4 class="modal-title" id="myModalLabel">Nouveau produit</h4>
+              </div>
+              <div class="modal-body">
+                {!! Form::open(array('id' => 'frm', 'name' => 'frm', 'class' => 'form-horizontal')) !!}
+                <input id="token" type="hidden" value="{{$encrypted_token}}">
+                @include('product.form')
+                {!! Form::close() !!}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
+      <!-- Modal -->
     </div>
-
   </div>
+</div>
+
 @endsection
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/t/dt/dt-1.10.11/datatables.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+  $('#example').DataTable();
+});
+</script>
